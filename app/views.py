0405070ba914e115bdd01dc80b866affe6ebe494
KicaRonaldOkello <okello.ronald045@gmail.com>
2018-09-22 18:59:27 +0300

    Add validation

--- app/views.py
+++ app/views.py
@@ -34,7 +34,10 @@ def get_all_orders():
     order_list = orders.get_orders()
     return jsonify({'orders': order_list}), 200
 
-@app.route("/api/v1/orders/<orderId>")
+@app.route("/api/v1/orders/<int:orderId>", methods = ["GET"])
 def get_one_order(orderId):
-    one_order = orders.get_an_order(orderId)
-    return jsonify({"order": one_order}),200
+    if isinstance(orderId,int):
+        one_order = orders.get_an_order(orderId)
+        return jsonify({"order": one_order}),200
+    else:
+        return jsonify({"Error": "Please input correct order id"}), 400
