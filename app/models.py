37644d9c731f519636f63fb12991a731f4dacc2c
KicaRonaldOkello <okello.ronald045@gmail.com>
2018-09-24 14:29:43 +0300

    Correct order method

--- app/models.py
+++ app/models.py
@@ -1,41 +1,6 @@
 from app import app
 from flask import jsonify
 
-class Order():
-    ORDER = []
-    def add_orders(self, order):
-        order["order_no"] = (len(self.ORDER) + 1)
-        order["order_status"] = "Pending"
-        self.ORDER.append(order)
-        return order
-
-    def get_orders(self):
-        ans  = Menu()
-        for food in self.ORDER:
-            food_names = ans.food_name(food["food"])
-            food["food"] = food_names
-        return self.ORDER
-
-    def get_an_order(self, orderId):
-        ans = Menu()
-        for order in self.ORDER:
-            if order["order_no"] == orderId:
-                food_name = ans.food_name(order["food"])
-                order["food"] = food_name
-            return order
-
-    def update_order_status(self, orderId):
-        for status in self.ORDER:
-            if status["order_no"] == orderId:
-                status["order_status"] = "Accepted"
-            return status
-        else:
-            return "does not exist"
-
-
-
-
-        
 class Menu:
     MENU = []
     def add_menu_item(self, menu):
@@ -50,3 +15,42 @@ class Menu:
         for food_id in self.MENU:
             if food_id["food_id"] == id:
                 return food_id["name"]
+
+class Order(Menu):
+    
+    ORDER = []
+    def add_orders(self, order):
+        order["order_no"] = (len(self.ORDER) + 1)
+        order["order_status"] = "Pending"
+        self.ORDER.append(order)
+        return order
+
+    def get_orders(self):
+        ORDERS = self.ORDER[:]
+        for food in ORDERS:
+            if isinstance(food["food"], int):
+                food_names = self.food_name(food["food"])
+                food["food"] = food_names
+                return ORDERS
+            else:
+                return ORDERS
+
+    def get_an_order(self, orderId):
+        for order in self.ORDER:
+            if order["order_no"] == orderId:
+                food_name = self.food_name(order["food"])
+                order["food"] = food_name
+            return order
+
+    def update_order_status(self, orderId, order_status):
+        update_order = [status for status in self.ORDER if status["order_no"] == orderId]
+        if update_order:
+            update_order[0]["order_status"] = order_status
+            return update_order
+        else:
+            return "does not exist"
+
+
+
+
+        
