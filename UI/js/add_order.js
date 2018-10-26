42eff5ece6244b760291d606892cd49080620dc6
KicaRonaldOkello <okello.ronald045@gmail.com>
2018-10-26 03:23:30 +0300

    Fix merge conflict, remove redundant form

--- UI/js/add_order.js
+++ UI/js/add_order.js
@@ -3,7 +3,11 @@ function pick_input(input){
     x = input.value;
 }
 
-function add_order(menu_id) {   
+function add_order(menu_id) {
+    var v = sessionStorage.getItem('Token');
+    if (v == null){
+        window.location.href = 'index.html';
+    };   
     var order_body = JSON.stringify({
         'amount': x,
         'food': menu_id.value
