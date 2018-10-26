8c5fb29e49c5312f0640369977d6068c10b43fb7
KicaRonaldOkello <okello.ronald045@gmail.com>
2018-10-26 07:51:19 +0300

    [Feature #161364509]Correct add menu.

--- UI/js/add_menu.js
+++ UI/js/add_menu.js
@@ -1,5 +1,5 @@
 function add_menu (){
-    sessionStorage.setItem('Token', 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTU0MDE0MTg1MSwiZXhwIjoxNTQwMTQyNzUxLCJpZGVudGl0eSI6eyJ1c2VybmFtZSI6InJvbm55IiwicGFzc3dvcmQiOiJwYmtkZjI6c2hhMjU2OjUwMDAwJFd2S0ZSRjM2JDlhMjViYWYxOWVkYjUwMmMyYjIwNDQ0ODdmYWFmZDFjMTVmM2Y1NjhmOGJmOTYyYzBmYjg2ZTk0ZDE4OGExNjEiLCJyb2xlIjoiYWRtaW4iLCJ1c2VyX2lkIjo0NH0sInR5cGUiOiJhY2Nlc3MiLCJqdGkiOiI4YzY0NDlhYS0wOTg4LTQxMzUtOGFhMi04MDFjZWRiNGFiYmIiLCJuYmYiOjE1NDAxNDE4NTF9.-0gqxGU0X5eR6q9OIkDhN4tqf3aVuwng1Hi8CRqDZB8');
+
     var menu_body = JSON.stringify({
         'name': document.getElementById("add_food").value,
         'price': document.getElementById("add_price").value,
